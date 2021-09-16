2aa604b21b872f504016c5954df3844e2d49a53d
Vladislav Likhachev <mymindissharp@gmail.com>
2021-09-16 10:49:41 +0300

    Добавил исправления

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,12 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.*;
 
 public class Main {
+    final static int THREADS_COUNT = 4;
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         task1();
@@ -13,16 +15,11 @@ public class Main {
 
     private static void task1() throws InterruptedException {
         ThreadGroup threadGroup = new ThreadGroup("группа");
-
-        Thread thread1 = new MyThread(threadGroup, "поток 1");
-        Thread thread2 = new MyThread(threadGroup, "поток 2");
-        Thread thread3 = new MyThread(threadGroup, "поток 3");
-        Thread thread4 = new MyThread(threadGroup, "поток 4");
-
-        thread1.start();
-        thread2.start();
-        thread3.start();
-        thread4.start();
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < THREADS_COUNT; i++) {
+            threads.add(new MyThread(threadGroup, "поток "+ (i + 1)));
+        }
+        threads.forEach(Thread::start);
 
         Thread.sleep(10000);
 
@@ -31,17 +28,14 @@ public class Main {
 
     private static void task2() throws InterruptedException, ExecutionException {
         CallableThread callableThread1 = new CallableThread();
-        final ExecutorService threadPool = Executors.newFixedThreadPool(4);
+        final ExecutorService threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
         final Future<Integer> task = threadPool.submit(callableThread1);// Получаем результат
         final Integer resultOfTask = task.get();
         System.out.printf("Количество сообщений: %s\n", resultOfTask);
 
         System.out.println("Все задачи");
-        CallableThread callableThread2 = new CallableThread();
-        CallableThread callableThread3 = new CallableThread();
-        CallableThread callableThread4 = new CallableThread();
 
-        List<Callable<Integer>> callables = Arrays.asList(callableThread1, callableThread2, callableThread3, callableThread4);
+        List<Callable<Integer>> callables = Arrays.asList(new CallableThread(), new CallableThread(), new CallableThread(), new CallableThread());
         List<Future<Integer>> list = threadPool.invokeAll(callables);
         list.forEach(g -> {
             try {
